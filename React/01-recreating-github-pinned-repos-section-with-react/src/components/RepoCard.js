0b65e175b03f61d56b67a604b00bdf8aac147429
Bhavesh <bhic2030@gmail.com>
2021-08-13 11:03:39 +0530

    Adding requirements.txt file

---
+++ React/01-recreating-github-pinned-repos-section-with-react/src/components/RepoCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactComponent as Svg } from "./repo.svg";
+
+const RepoCard = ()=>{
+    return (
+        <>
+        <h2>I am repocard</h2>
+        <Svg></Svg>
+        </>
+    )
+}
+
+export default RepoCard;
---
+++ React/01-recreating-github-pinned-repos-section-with-react/src/components/data.js
@@ -0,0 +1,8 @@
+export default [
+  {
+    repoName: "1-Hour-Mini-Projects",
+    description:
+      "A Repository for Mini-Projects which can be done within an hour. ",
+  },
+];
+
