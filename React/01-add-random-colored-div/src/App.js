f484fb8732312b63ee6cf9145f076656c5c3cd66
Bhavesh <bhic2030@gmail.com>
2021-08-15 02:15:03 +0530

    Adding Styles for react project

--- React/01-add-random-colored-div/src/App.js
+++ React/01-add-random-colored-div/src/App.js
@@ -1,77 +1,95 @@
-import React,{useState} from 'react'
-import './App.css'
-import Div from './Components/Div'
+import React, { useState} from "react";
+import "./App.css";
+import Div from "./Components/Div";
 
-const App = ()=>{
+const App = () => {
+  // useState() for keeping account of number of Divs
+  const [count, setCount] = useState(2);
+  // An array for storing all the current Div Components
+  const options = [];
 
-    const [count,setCount] = useState(2)
-    const options = [];
+  const getRandomColor = () => {
+    var colorArray = [
+      "fuchsia",
+      "tomato",
+      "coral",
+      "yellow",
+      "red",
+      "aqua",
+      "lime",
+      "lightcyan",
+      "turquoise",
+      "dodgerblue",
+      "violet",
+      "deeppink",
+      "lightpink",
+      "beige",
+      "bisque",
+      "cornsilk",
+      "linen",
+      "mintcream",
+      "seashell",
+      "honeydew",
+      "azure",
+      "whitesmoke",
+      "crimson",
+      "blueviolet",
+      "gold",
+      "blanchedalmond",
+      "palegreen",
+      "mediumspringgreen",
+      "lightyellow",
+      "moccasin",
+      "mistyrose",
+      "lavenderblush",
+      "lavender",
+      "oldlace",
+      "aliceblue",
+      "ivory",
+      "snow",
+      "gainsboro",
+    ];
+    let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)];
+    return randomColor;
+  };
 
-    const getRandomColor=()=> {
-     var colorArray = [
-       "fuchsia",
-       "tomato",
-       "coral",
-       "yellow",
-       "red",
-       "aqua",
-       "lime",
-       "lightcyan",
-       "turquoise",
-       "dodgerblue",
-       "violet",
-       "deeppink",
-       "lightpink",
-       "beige",
-       "bisque",
-       "cornsilk",
-       "linen",
-       "mintcream",
-       "seashell",
-       "honeydew",
-       "azure",
-       "whitesmoke",
-       "crimson",
-       "blueviolet",
-       "gold",
-       "blanchedalmond",
-       "palegreen",
-       "mediumspringgreen",
-       "lightyellow",
-       "moccasin",
-       "mistyrose",
-       "lavenderblush",
-       "lavender",
-       "oldlace",
-       "aliceblue",
-       "ivory",
-       "snow",
-       "gainsboro"
-     ];
-      let randomColor = colorArray[Math.floor(Math.random()*colorArray.length)]
-      return randomColor;
-    }
-
-    for(let i = 0; i < count;i++){
-        var color = getRandomColor()
-        options.push(<Div colorName={color} number={i + 1} color ={color} key={i} />)
-    }
-
-    const addDiv = ()=>{
-        setCount(count + 1)
-    }
-
-    return (
-      <>
-        <div className="container">
-          <button className="btn" onClick={()=>addDiv()} >Add Div</button>
-          <div className="content">
-            {options}
-          </div>
-          <button className="btn" onClick={()=>setCount(0)} >Clear All</button>
-        </div>
-      </>
+  for (let i = 0; i < count; i++) {
+    var color = getRandomColor();
+    options.push(
+      <Div colorName={color} number={i + 1} color={color} key={i} />
     );
-}
+  }
+
+  const addDiv = () => {
+    setCount(count + 1);
+  };
+
+  // Conditional Rendering
+  const Para = () => {
+    if (options.length === 0) {
+      return <p>All Divs Were Cleared.</p>;
+    } else if (options.length === 1) {
+      return <p>There is 1 Div.</p>;
+    } else {
+      return <p>There are {options.length} Divs.</p>;
+    }
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>Add Random Colored Div</h1>
+        <Para></Para>
+        <button className="btn" onClick={() => addDiv()}>
+          Add Div
+        </button>
+        <div className="content">{options}</div>
+        <button className="btn" onClick={() => setCount(0)}>
+          Clear All
+        </button>
+      </div>
+    </>
+  );
+};
 
 export default App;
