51e815f66a497d4f5f0deaa6b3f5efe5eb140143
Bhavesh <bhic2030@gmail.com>
2021-08-20 00:45:18 +0530

    fixing the bug at t = 0 Project08 JS-HTML-CSS

--- JS-HTML-CSS/08-Quiz-in-Video/script.js
+++ JS-HTML-CSS/08-Quiz-in-Video/script.js
@@ -17,11 +17,13 @@ const setOptions = () => {
 videoPlayer.addEventListener('loadeddata',()=>setOptions())
 
 const setTime = () => {
+  console.log(select.value)
   timeCount.style.opacity = 1;
   timeCount.innerHTML = `<p>The Question will appear in ${select.value} second/s</p>`;
   videoPlayer.currentTime = 0;
   question.style.display = "none";
   videoPlayer.play();
+  manageInterval()
 };
 
 setTimeBtn.addEventListener("click", () => setTime());
@@ -36,10 +38,13 @@ const showQuestion = (time) => {
 };
 
 const manageInterval = ()=>{
-  if (Math.floor(videoPlayer.currentTime) > select.value) {
+  console.log("this is manage interval");
+  if (Math.floor(videoPlayer.currentTime) > select.value && select.value != 0) {
+    console.log("fall");
     clearInterval(myInterval);
   }
   if (Math.floor(videoPlayer.currentTime) < select.value) {
+    console.log("call");
     myInterval = setInterval(
       () => showQuestion(parseInt(select.value, 10)),
       1000
@@ -49,7 +54,11 @@ const manageInterval = ()=>{
 
 videoPlayer.onplay = ()=>{
   manageInterval()
-
+  if (select.value == 0 && videoPlayer.currentTime < 1) {
+    question.style.display = "block";
+    videoPlayer.pause();
+    videoPlayer.currentTime = 1;
+  }
 }
 
 continueBtn.addEventListener("click", () => continueVideo());
@@ -68,3 +77,8 @@ const checkAnswer = () => {
     review.innerHTML = "Your answer was wrong, Please try again !";
   }
 };
+
+
+// Bugs 
+// 1. doesn't works at t = 0 (update : bug is almost fixed but we have to run the myInterval twice to work things normally)
+// 2. controls by keyboard
