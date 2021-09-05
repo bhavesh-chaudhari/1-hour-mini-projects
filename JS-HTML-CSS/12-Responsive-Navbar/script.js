84d82749e2613e3bbb46891f1df9f72083b95b52
Bhavesh <bhic2030@gmail.com>
2021-09-05 20:20:28 +0530

    styles update

--- JS-HTML-CSS/12-Responsive-Navbar/script.js
+++ JS-HTML-CSS/12-Responsive-Navbar/script.js
@@ -2,6 +2,7 @@ const mobile = document.getElementById("mobile")
 const navbar = document.getElementById("navbar")
 const open = document.getElementById("open");
 const close = document.getElementById("close");
+const content = document.getElementById("content")
 
 open.addEventListener("click", ()=>{
     navbar.style.transform = "translate(0,0)";
@@ -18,3 +19,21 @@ close.addEventListener("click",()=>{
     navbar.style.transform = "translate(0,-100%)";
     document.querySelector("body").style.overflowY = "auto";
 })
+
+function Reset(x) {
+  if (x.matches) { // If media query matches
+    mobile.style.display = "none"
+    navbar.style.transform = "translate(0,0)";
+    document.querySelector("body").style.overflowY = "auto";
+    close.style.display = "none";
+  } 
+  else{
+    mobile.style.display = "block";
+    navbar.style.transform = "translate(0,-100%)";
+    open.style.display = "block";
+  }
+}
+
+var x = window.matchMedia("(min-width: 700px)")
+Reset(x) 
+x.addListener(Reset) 
