c48eb05ca19c91afa572c8b544258778511bcb8c
Bhavesh <bhic2030@gmail.com>
2021-09-05 11:20:39 +0530

    project12 styles completed

--- JS-HTML-CSS/12-Responsive-Navbar/script.js
+++ JS-HTML-CSS/12-Responsive-Navbar/script.js
@@ -1,9 +1,20 @@
-const hamburgerBtn = document.getElementById("hamburger-btn")
 const mobile = document.getElementById("mobile")
 const navbar = document.getElementById("navbar")
+const open = document.getElementById("open");
+const close = document.getElementById("close");
 
-hamburgerBtn.addEventListener("click", ()=>{
-    console.log("clicked");
-    navbar.style.display = "block"
+open.addEventListener("click", ()=>{
+    navbar.style.transform = "translate(0,0)";
     mobile.style.display = "none"
+    open.style.display = "none";
+    close.style.display = "block"
+    document.querySelector("body").style.overflowY = "hidden"
+})
+
+close.addEventListener("click",()=>{
+    close.style.display = "none";
+    open.style.display = "block"
+    mobile.style.display = "block";
+    navbar.style.transform = "translate(0,-100%)";
+    document.querySelector("body").style.overflowY = "auto";
 })
